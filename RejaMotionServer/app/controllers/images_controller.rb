ce25cda40efeaa4481feb7469722686f461c76a1
Kentaro Takiguchi <takiguchi0817@gmail.com>
2013-03-25 22:59:00 +0900

    Show random selected images

--- RejaMotionServer/app/controllers/images_controller.rb
+++ RejaMotionServer/app/controllers/images_controller.rb
@@ -1,8 +1,13 @@
 class ImagesController < ApplicationController
+
   def index
-    @image_url = "http://rejamotion.com/images/gifs/88.gif"
-    @image_title = "Frash Fish"
+    @image_list = Image.find(:all).sample(6)
+
+    @image_url = @image_list[0].url
+    @image_title = @image_list[0].title
     @image_text = "Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit."
+
     render 'images/index'
   end
+
 end
