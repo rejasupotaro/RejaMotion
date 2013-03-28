4a5a19072d2554005f2c21a1538a4fe70b712f73
Kentaro Takiguchi <takiguchi0817@gmail.com>
2013-03-28 14:54:39 +0900

    Delgate zip_to_gif to images helper

--- RejaMotionServer/app/controllers/images_controller.rb
+++ RejaMotionServer/app/controllers/images_controller.rb
@@ -1,6 +1,6 @@
-require 'zipruby'
-
 class ImagesController < ApplicationController
+
+  include ImagesHelper
 
   def index
     #@image_list = Image.find(:all, :order => 'created_at, id desc', :limit => 6)
@@ -19,57 +19,19 @@ class ImagesController < ApplicationController
 
   def upload
     image_title = params[:image_title]
-    content = params[:content]
+    image_url = '/images/' + generate_image_file_name
+
     zipfile = "out.zip"
     zipfile_path = Rails.root.to_s + "/tmp/" + zipfile
-    File.binwrite(zipfile_path, content.tempfile.read)
+    out_path = Rails.root.to_s + '/public' + image_url
 
-    upload_time = Time.now
-    upload_time_unix = upload_time.to_i
-    image_url = '/images/' + upload_time_unix.to_s + '.gif'
-    output_path = Rails.root.to_s + '/public' + image_url
-
-    zip_to_gif(zipfile_path, output_path, params[:delay])
+    zip_to_gif(params[:content], zipfile_path, out_path, params[:delay])
 
     Image.create(
       :title      => image_title,
       :url        => image_url)
 
     render :json => {"result" => "ok"}
-  end
-
-  def zip_to_gif(src_path, out_path, delay)
-    image_path_array = Array.new
-    tmpout_path = Rails.root.to_s + "/tmp/out/"
-    Zip::Archive.open(src_path.to_s) do |archives|
-      archives.each do |archive|
-        FileUtils.makedirs(tmpout_path)
-        unless archive.directory?
-          image_path = tmpout_path + archive.name
-          File.open(image_path, "w+b") do |file|
-            file.print(archive.read)
-          end
-          image_path_array.push(image_path)
-        end
-      end
-    end
-
-    convert_command = convert_command_builder(image_path_array, out_path, delay)
-    system(convert_command)
-    remove_command = 'rm -rf ' + Rails.root.to_s + '/tmp/out/*'
-    system(remove_command)
-  end
-
-  def convert_command_builder(image_path_array, out_path, delay, max_size = 600)
-    resize_option = '-resize ' + max_size.to_s + 'x' + max_size.to_s
-    delay_option = '-delay ' + delay.to_s
-
-    image_list_string = '';
-    image_path_array.each do |image_path|
-      image_list_string << image_path.to_s << ' '
-    end
-
-    return 'convert ' + resize_option + ' ' + delay_option + ' ' + image_list_string + out_path.to_s
   end
 
 end
--- RejaMotionServer/app/helpers/images_helper.rb
+++ RejaMotionServer/app/helpers/images_helper.rb
@@ -1,2 +1,47 @@
+require 'zipruby'
+
 module ImagesHelper
+
+  def generate_image_file_name
+    upload_time = Time.now
+    upload_time_unix = upload_time.to_i
+    upload_time_unix.to_s + ".gif"
+  end
+
+  def zip_to_gif(content, zipfile_path, out_path, delay)
+    File.binwrite(zipfile_path, content.tempfile.read)
+
+    image_path_array = Array.new
+    tmpout_path = Rails.root.to_s + "/tmp/out/"
+    Zip::Archive.open(zipfile_path.to_s) do |archives|
+      archives.each do |archive|
+        FileUtils.makedirs(tmpout_path)
+        unless archive.directory?
+          image_path = tmpout_path + archive.name
+          File.open(image_path, "w+b") do |file|
+            file.print(archive.read)
+          end
+          image_path_array.push(image_path)
+        end
+      end
+    end
+
+    convert_command = convert_command_builder(image_path_array, out_path, delay)
+    system(convert_command)
+    remove_command = 'rm -rf ' + Rails.root.to_s + '/tmp/out/*'
+    system(remove_command)
+  end
+
+  def convert_command_builder(image_path_array, out_path, delay, max_size = 600)
+    resize_option = '-resize ' + max_size.to_s + 'x' + max_size.to_s
+    delay_option = '-delay ' + delay.to_s
+
+    image_list_string = '';
+    image_path_array.each do |image_path|
+      image_list_string << image_path.to_s << ' '
+    end
+
+    return 'convert ' + resize_option + ' ' + delay_option + ' ' + image_list_string + out_path.to_s
+  end
+
 end
