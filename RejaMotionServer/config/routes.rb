abef3effcee6f4eea6b79908c7becbc636226741
Kentaro Takiguchi <takiguchi0817@gmail.com>
2013-04-17 00:02:11 +0900

    Change template engine from erb to haml

--- RejaMotionServer/config/routes.rb
+++ RejaMotionServer/config/routes.rb
@@ -1,6 +1,5 @@
 RejaMotionServer::Application.routes.draw do
   root :to => 'images#index'
-  match 'images/:order' => 'images#index'
   resources :images, :only => [:index, :show]
   match 'api/upload' => 'images#upload'
 end
