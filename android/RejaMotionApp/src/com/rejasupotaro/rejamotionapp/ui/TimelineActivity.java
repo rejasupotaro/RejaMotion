0ca79859d05ec91df646d73577cc2e7cc806a6ac
Kentaro Takiguchi <takiguchi0817@gmail.com>
2013-05-12 10:35:26 +0900

    Change reference for Hybridge

--- android/RejaMotionApp/src/com/rejasupotaro/rejamotionapp/ui/TimelineActivity.java
+++ android/RejaMotionApp/src/com/rejasupotaro/rejamotionapp/ui/TimelineActivity.java
@@ -2,9 +2,6 @@ package com.rejasupotaro.rejamotionapp.ui;
 
 import javax.inject.Inject;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import proton.inject.activity.ProtonActivity;
 import android.graphics.Bitmap;
 import android.graphics.Picture;
@@ -17,46 +14,23 @@ import android.view.MenuItem;
 import android.view.MenuItem.OnMenuItemClickListener;
 import android.view.View;
 import android.webkit.WebChromeClient;
-import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebView.PictureListener;
 import android.webkit.WebViewClient;
 import android.widget.ProgressBar;
 
+import com.rejasupotaro.hybridge.HybridgeWebView;
+import com.rejasupotaro.hybridge.annotation.Bridge;
 import com.rejasupotaro.rejamotionapp.Constants;
-import com.rejasupotaro.rejamotionapp.JavaScriptInterface;
 import com.rejasupotaro.rejamotionapp.R;
 import com.rejasupotaro.rejamotionapp.ui.helper.RejaMotionActivityHelper;
-import com.rejasupotaro.rejamotionapp.utils.ToastUtils;
 import com.rejasupotaro.rejamotionapp.utils.UriUtils;
 
 public class TimelineActivity extends ProtonActivity {
-    private static final String TAG = TimelineActivity.class.getSimpleName();
 
-    private WebView mWebView;
+    private HybridgeWebView mWebView;
     private ProgressBar mProgressLoading;
     @Inject private RejaMotionActivityHelper mActivityHelper;
-    private JavaScriptInterface mJavaScriptInterface;
-
-    private JavaScriptInterface.Receiver mJavaScriptInterfaceReceiver = new JavaScriptInterface.Receiver() {
-        public void receive(JSONObject jsonObject) {
-            try {
-                JSONObject body = jsonObject.getJSONObject("body");
-                Log.d(TAG, body.toString());
-                if (body.has("title")) {
-                    String title = body.getString("title");
-                    ToastUtils.show(TimelineActivity.this, title + "にLikeしました");
-                } else if (body.has("image_url")) {
-                    String image_url = body.getString("image_url");
-                    ToastUtils.show(TimelineActivity.this, image_url);
-                } else {
-                    Log.v(TAG, "Received unknown body. Check your code.");
-                }
-            } catch (JSONException e) {
-                Log.d(TAG, jsonObject.toString(), e);
-            }
-        }
-    };
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -64,28 +38,19 @@ public class TimelineActivity extends ProtonActivity {
         setContentView(R.layout.activity_timeline);
         getViews();
 
-        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.init(this);
         setupWebViewClient(mWebView);
-        setupWebViewCache(mWebView);
         mWebView.loadUrl(Constants.APP_SITE_URL);
-
-        mJavaScriptInterface = new JavaScriptInterface(mWebView, mJavaScriptInterfaceReceiver);
 
         mActivityHelper.setupSplashAnimation(new Handler());
     }
 
     private void getViews() {
-        mWebView = (WebView) findViewById(R.id.webview_timeline);
+        mWebView = (HybridgeWebView) findViewById(R.id.webview_timeline);
         mProgressLoading = (ProgressBar) findViewById(R.id.progress_loading);
     }
 
-    private void setupWebViewCache(WebView webView) {
-        //webView.clearCache(boolean); true => delete RAM and DB, false => delete RAM
-        webView.getSettings().setAppCacheEnabled(true);
-        webView.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
-    }
-
-    private void setupWebViewClient(WebView webView) {
+    private void setupWebViewClient(HybridgeWebView webView) {
         webView.setWebChromeClient(new WebChromeClient() {});
 
         webView.setWebViewClient(new WebViewClient(){
@@ -144,5 +109,10 @@ public class TimelineActivity extends ProtonActivity {
             }
         });
         return true;
+    }
+
+    @Bridge("system.log")
+    public void log() {
+        Log.d("DEBUG", "system.log");
     }
 }
